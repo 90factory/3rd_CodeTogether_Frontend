03f0898f18bed5ca3c18886e8af919503e582c5b
bomee <primavera0331@google.com>
2020-04-10 19:27:22 +0900

    init commit

---
+++ src/js/defualt.js
@@ -0,0 +1,7 @@
+document.getElementById("header").innerHTML='<object type="text/html" data="/src/html/default/header.html"></object>';
+
+document.getElementById("main").innerHTML='<object type="text/html" data="/src/html/main/main-page.html"></object>';
+
+// document.getElementById("lecture-search").innerHTML='<object type="text/html" data="/src/html/lecture/lecture-search.html"></object>';
+
+document.getElementById("footer").innerHTML='<object type="text/html" data="/src/html/default/footer.html"></object>';
